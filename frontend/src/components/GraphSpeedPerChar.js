68923b06b5eb8e2309182eac451731f90cbd2efe
lesliesage <leslie.sage@gmail.com>
2019-09-23 14:18:09 -0400

    plotted test data; formatting in pr ogress

--- frontend/src/components/GraphSpeedPerChar.js
+++ frontend/src/components/GraphSpeedPerChar.js
@@ -3,48 +3,134 @@ import { withRouter } from "react-router-dom";
 import { connect } from "react-redux";
 import { Bar } from "react-chartjs-2";
 
-const data = {
-  labels: ["January", "February", "March", "April", "May", "June", "July"],
-  datasets: [
-    {
-      label: "My First dataset",
-      backgroundColor: "rgba(255,99,132,0.2)",
-      borderColor: "rgba(255,99,132,1)",
-      borderWidth: 1,
-      hoverBackgroundColor: "rgba(255,99,132,0.4)",
-      hoverBorderColor: "rgba(255,99,132,1)",
-      data: [65, 59, 80, 81, 56, 55, 40]
-    }
-  ]
-};
-
-// const data = props => {
-//   console.log(props)
-//   return {
-//     labels: props.allTests.last.keys,
-//     datasets: [
-//       {
-//         label: "My First dataset",
-//         backgroundColor: "rgba(255,99,132,0.2)",
-//         borderColor: "rgba(255,99,132,1)",
-//         borderWidth: 1,
-//         hoverBackgroundColor: "rgba(255,99,132,0.4)",
-//         hoverBorderColor: "rgba(255,99,132,1)",
-//         data: props.allTests.last.values
-//       }
-//     ]
-//   };
-// };
-
 const GraphSpeedPerChar = props => {
+  const FIELDKEY = {
+    alpha: "a-z",
+    numeric: "0-9",
+    tilde: "~",
+    backtick: "`",
+    exclamation: "!",
+    at: "@",
+    octothorpe: "#",
+    dollar: "$",
+    percent: "%",
+    carrot: "^",
+    ampersand: "&",
+    star: "*",
+    open_paren: "(",
+    close_paren: ")",
+    long_dash: "_",
+    dash: "-",
+    plus: "+",
+    equals: "=",
+    open_curly: "{",
+    close_curly: "}",
+    open_bracket: "[",
+    close_bracket: "]",
+    pipe: "|",
+    backslash: "\\",
+    colon: ":",
+    semicolon: ";",
+    doublequote: '"',
+    singlequote: "'",
+    open_angle: "<",
+    close_angle: ">",
+    comma: ",",
+    period: ".",
+    question: "?",
+    slash: "/"
+  };
+
+  function arrayify(obj) {
+    // turn test result into object, remove metadata (id, created_at, user_id, etc.)
+    const arr = Object.entries(obj).filter(el => {
+      return Object.keys(FIELDKEY).includes(el[0]);
+    });
+    // remove unused characters and sort ascending by typing speed
+    return arr
+      .filter(el => el[1])
+      .sort((a, b) => {
+        return a[1] > b[1] ? 1 : -1;
+      });
+  }
+
+  function getKeys(arr) {
+    // return keys for x-axis, replacing char's text label with symbol
+    return arr.map(el => {
+      return FIELDKEY[el[0]];
+    });
+  }
+
+  function getValues(arr) {
+    // return values for y-axis, rounding to integer
+    return arr.map(el => {
+      return Math.round(el[1]);
+    });
+  }
+
+  const data = {
+    labels: getKeys(arrayify(props.testSummary)),
+    datasets: [
+      {
+        // label: ,
+        backgroundColor: "lightgrey",
+        borderColor: "grey",
+        borderWidth: 0,
+        hoverBackgroundColor: "grey",
+        hoverBorderColor: "black",
+        data: getValues(arrayify(props.testSummary)),
+        legend: {
+          display: false
+        },
+        options: {
+          title: {
+            display: false
+          },
+          legend: {
+            display: false
+          },
+          scales: {
+            xAxes: [
+              {
+                ticks: {
+                  display: false
+                },
+                gridLines: {
+                  color: "rgba(0, 0, 0, 0)",
+                  linewidth: 0,
+                  drawOnChartArea: false,
+                  display: false
+                }
+              }
+            ],
+            yAxes: [
+              {
+                ticks: {
+                  display: false
+                },
+                gridLines: {
+                  color: "rgba(0, 0, 0, 0)",
+                  linewidth: 0,
+                  drawOnChartArea: false,
+                  display: false
+                }
+              }
+            ]
+          }
+        }
+      }
+    ]
+  };
+
   return (
     <div>
       <h2>GraphSpeedPerChar</h2>
       <Bar
         data={data}
-        width={100}
-        height={50}
-        options={{ maintainAspectRatio: false }}
+        // width={100}
+        // height={50}
+        // options={{ maintainAspectRatio: false }}
+        // options={options}
       />
     </div>
   );
@@ -54,11 +140,10 @@ const mapStateToProps = state => {
   return {
     selectedSnippet: state.test.selectedSnippet,
     currentTestResults: state.test.currentTestResults,
-    savedTest: state.test.testSummary,
+    testSummary: state.test.testSummary,
     allTests: state.stats.allTests
   };
 };
 
 export default connect(mapStateToProps)(GraphSpeedPerChar);
-
 // export default withRouter(GraphSpeedPerChar);
