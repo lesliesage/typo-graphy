48e6c605cb40ce87516aae88261b9e72a4965a47
lesliesage <leslie.sage@gmail.com>
2020-05-24 21:37:56 -0400

    created reset page

---
+++ src/components/Reset.js
@@ -0,0 +1,97 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { URL_RESET } from "../constants/constants.js";
+import { updateUser } from "../redux/actions";
+import { Redirect } from "react-router-dom";
+
+class Reset extends Component {
+  state = { email: "", token: "", password: "", redirect: false };
+
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  handleSubmitReset = (e) => {
+    e.preventDefault();
+    fetch(URL_RESET, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        email: this.state.email,
+        token: "56ac31db0cf4e6b5df90", //TODO: replace with query params
+        password: this.state.password,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.jwt) {
+          localStorage.setItem("token", data.jwt);
+          this.props.updateUser(JSON.parse(data.user));
+          this.setState({ redirect: true });
+        } else {
+          alert("invalid reset");
+        }
+      });
+  };
+
+  render() {
+    return this.state.redirect ? (
+      <Redirect to="/" />
+    ) : (
+      <div className="main info-pg reset">
+        <h1>reset</h1>
+        <div className="form-container">
+          <div className="form-labels-container">
+            <div className="form-label">email:</div>
+            <div className="form-label">new password:</div>
+          </div>
+          <form
+            className="form-inputs-container"
+            onSubmit={this.handleSubmitReset}
+          >
+            <input
+              className="form-input"
+              id="email"
+              name="email"
+              onChange={this.handleChange}
+              value={this.state.email}
+              placeholder="email"
+            ></input>
+            <br />
+            <input
+              type="password"
+              className="form-input"
+              id="password"
+              name="password"
+              onChange={this.handleChange}
+              value={this.state.password}
+              placeholder="password"
+            ></input>
+            <br />
+            <button type="submit" className="btn">
+              save
+            </button>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    user: state.user,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    updateUser: (user) => {
+      dispatch(updateUser(user));
+    },
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Reset);
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -6,10 +6,11 @@ export const URL_TESTS = API_URL + "tests";
 export const URL_MEDIANS = API_URL + "medians";
 export const URL_HELP_ARTICLES = API_URL + "help_articles";
 export const URL_LOGIN = API_URL + "login";
-export const URL_FORGOT = API_URL + "forgot";
 export const URL_SIGNUP = API_URL + "signup";
 export const URL_LOGOUT = API_URL + "logout";
 export const URL_PROFILE = API_URL + "profile";
+export const URL_FORGOT = API_URL + "forgot";
+export const URL_RESET = API_URL + "reset";
 export const URL_USERS_UPDATE = API_URL + "users/update";
 export const URL_USERS_DESTROY = API_URL + "users/destroy";
 
